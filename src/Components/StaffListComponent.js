8ae0786673469cd6803feb4568ab8c1277af0181
Xuan Loc <Xuanloc1302@gmail.com>
2022-09-27 21:45:42 +0700

    commit next

--- src/Components/StaffListComponent.js
+++ src/Components/StaffListComponent.js
@@ -1,34 +1,129 @@
-import React from "react";
+import React, { Component } from "react";
 import {
     Card,
     CardImg,
     CardBody,
-    CardSubtitle
+    CardSubtitle,
+    Button,
+    Modal,
+    Col,
+    Input,
+    ModalHeader,
+    ModalBody,
+    Row,
+    Label,
+    FormFeedback
 } from "reactstrap";
-import { NavLink } from "react-router-dom";
+import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
 
-function StaffList(props) {
+const RenderStaffItem = ({ staff }) => {
+    return (
+        <Link to={`/staff/${staff.id}`}>
+            <Card>
+                <CardImg width="100%" src={staff.image} alt={staff.name} />
+                <CardBody>
+                    <CardSubtitle>{staff.name}</CardSubtitle>
+                </CardBody>
+            </Card>
+        </Link>
+    );
+};
+class StaffList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nameF: "",
+            modalOpen: false,
+            doB: "",
+            touched: {
+                doB: false,
+                startDate: false
+            }
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.timNhanvien = this.timNhanvien.bind(this);
+
+    }
+
+    toggleModal() {
+        this.setState({
+            modalOpen: !this.state.modalOpen
+        });
+    }
+
+    timNhanvien(event) {
+        const nameS = event.target.nameS.value;
+        event.preventDefault();
+        this.setState({ nameF: nameS });
+    }
+    validate(doB, startDate) {
+        const errors = {
+          doB: "",
+          startDate: ""
+        };
     
-    const ListNhanvien = props.staffs.map((nv) => {
+        if (this.state.touched.doB && doB.length < 1) errors.doB = "Yêu cầu nhập";
+        if (this.state.touched.startDate && startDate.length < 1)
+          errors.startDate = "Yêu cầu nhập";
+    
+        return errors;
+      }
+    render() {
+        const errors = this.validate(this.state.doB, this.state.startDate); // Tạo biến báo lỗi khi người dùng khai báo thiếu
+        const staffList = this.props.staffs
+            .filter((val) => {
+                if (this.state.nameF === "") return val;
+                else if (
+                    val.name.toLowerCase().includes(this.state.nameF.toLowerCase())
+                )
+                    return val;
+                return 0;
+            })
+            .map((val) => {
+                return (
+                    <div className="col-6 col-md-4 col-lg-2 mt-3 mb-3" key={val.id}>
+                        <RenderStaffItem staff={val} />
+                    </div>
+                );
+            });
         return (
-            <div key={nv.id}>
-                <div className="col-12 col-md-2" >
-                    <img src={nv.image} alt={nv.name} />
-                    <NavLink className="nav-link" to={`/nhanvien/${nv.id}`}>
-                        <span ></span> { nv.name}
-                    </NavLink>
-                    {/* <p onClick={Render}>{nv.name }</p> */}
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-6 mt-3">
+                        <div className="row">
+                            <div className="col-10 col-md-10">
+                                <h3>Nhân viên</h3>
+                            </div>
+                            <div className="col-2 col-auto">
+                                <Button outline onClick={this.toggleModal}>
+                                    <span className="fa fa-plus fa-lg"></span>
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6 mt-3">
+                        <form onSubmit={this.timNhanvien} className="form-group row">
+                            <div className="col-8 col-md-8">
+                                <input
+                                    type="text"
+                                    name="nameS"
+                                    className="form-control"
+                                    placeholder="Tìm kiếm nhân viên ..."
+                                />
+                            </div>
+                            <div className="col-4 col-md-4">
+                                <button className="btn btn-success" type="submit">
+                                    Tìm kiếm
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        );
-    });
-    return <div className="container">
-        <div className="row">
-            {ListNhanvien}
-        </div>
-    </div>;
-
+        )
+    }
 }
 
 export default StaffList;
