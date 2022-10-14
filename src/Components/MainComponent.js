0a738ea554d7fafab960ab3a12d78c430d441ff5
Xuan Loc <Xuanloc1302@gmail.com>
2022-10-15 04:14:17 +0700

    commit 2

---
+++ src/Components/MainComponent.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Header from "./HeaderComponent";
+import Footer from "./footerComponent";
+import StaffList from "./StaffListComponent";
+import StaffDetail from "./StaffdetailComponent";
+import Salary from "./SalaryComponent";
+import Department from "./Department";
+import { STAFFS, DEPARTMENTS } from "../share/staffs";
+import { Route, Switch,  } from "react-router-dom";
+
+
+function Main() {
+    const [nhanvien, setNhanvien] = useState({
+        staffs: STAFFS,
+        departments: DEPARTMENTS
+    });
+
+    const StaffWithId = ({ match }) => {
+        return (
+            <StaffDetail
+                nv={
+                    nhanvien.staffs.filter(
+                        (staff) => staff.id === parseInt(match.params.nhanvienId, 10)
+                    )[0]
+                }
+            />
+        );
+    };
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/nhanvien/:nhanvienId" component={StaffWithId} />
+               
+                <Route
+                    path='/nhanvien'
+                    component={() => <StaffList staffs={nhanvien.staffs} />}>
+                </Route>
+                <Route
+                    path="/salary"
+                    component={() => <Salary staffs={nhanvien.staffs} />}
+                />
+                <Route
+                    path="/bophan"
+                    component={() =>(<Department departments={nhanvien.departments} />)}
+                />
+                
+            </Switch>
+            <Footer />
+        </div>
+    )
+}
+export default Main;
---
+++ src/Components/StaffListComponent.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+    Card,
+    CardImg,
+    CardBody,
+    CardSubtitle
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+function StaffList(props) {
+    
+    const ListNhanvien = props.staffs.map((nv) => {
+        return (
+            <div key={nv.id}>
+                <div className="col-12 col-md-2" >
+                    <img src={nv.image} alt={nv.name} />
+                    <NavLink className="nav-link" to={`/nhanvien/${nv.id}`}>
+                        <span ></span> { nv.name}
+                    </NavLink>
+                    {/* <p onClick={Render}>{nv.name }</p> */}
+                </div>
+            </div>
+        );
+    });
+    return <div className="container">
+        <div className="row">
+            {ListNhanvien}
+        </div>
+    </div>;
+
+}
+
+export default StaffList;
